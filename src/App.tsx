102a460ffca837588289474c3899267fb18c8b5a
Lucas Custodio <lucas.custodio@midway.com.br>
2025-12-09 20:11:00 -0300

    feat: add styled components

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
+
+export default function App() {
+  return (
+    <MyView>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </MyView>
+  );
+}
+
+const MyView = styled.View`
+  flex: 1;
+`;

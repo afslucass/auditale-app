c95a1157df16a1b6be14750d35493d585362bb2c
Lucas Custodio <lucas.custodio@midway.com.br>
2026-03-03 21:15:26 -0300

    refactor: improve code

--- story-creator-automation/audio.py
+++ story-creator-automation/audio.py
@@ -15,8 +15,6 @@ def generateStoryAudios():
     for index, item in enumerate(dados):
         tts.falar(item["chapter"], estilo=GENERATE_CHAPTER_AUDIO, arquivo=f"temp/chapter-{index}.wav")
         tts.falar(item["description"], estilo=GENERATE_CHAPTER_DESC_AUDIO, arquivo=f"temp/chapter-desc-{index}.wav")
-
-    for item in dados:
         for word in item["words"]:
             tts.falar(f"{word['word']} traduz para {word['translatedWord']}", estilo=GENERATE_NEW_WORDS_AUDIO, arquivo=f"temp/{word['id']}.wav")
 

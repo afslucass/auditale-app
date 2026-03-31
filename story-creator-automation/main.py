335e790e2ad8cba7ed5ced93ca439d0ee46c52ec
Lucas Francisco <afslucass@gmail.com>
2026-03-31 20:12:16 -0300

    feat: add clean temp

--- story-creator-automation/main.py
+++ story-creator-automation/main.py
@@ -2,8 +2,25 @@ from audio import convertAudioToMP3, generateStoryAudios, mergeStoryAudios
 from image import createStoryThumbnail
 from text import createStoryText, createStoryContentMetadata, createStoryInfo, mergeStoryContentMetadata
 from uploads import uploadImage, createWords, createStory
+import os
+import shutil
+
+def cleanTemp():
+    temp_dir = "temp"
+    if os.path.exists(temp_dir):
+        for filename in os.listdir(temp_dir):
+            file_path = os.path.join(temp_dir, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
 
 def setup():
+    cleanTemp()
+    
     createStoryText()
 
     generateStoryAudios()
